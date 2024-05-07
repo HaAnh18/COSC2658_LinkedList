d8493a6cb06ceb3f7741a65071a2363669385456
HaAnh18 <102302068+HaAnh18@users.noreply.github.com>
2024-05-07 15:22:33 +0700

    update linkedlist code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
     public static void main(String[] args) {
        Map2D map = new Map2D();
-       PlaceManager.loadPlacesFromFile(map, "Cluster.csv",1000000);
-       System.out.println(map.size());
+       map.add("1", "1",1, 4, new String[] {"Restaurant"});
+        map.add("2", "2",2, 6, new String[] {"Restaurant"});
+        map.add("3", "3",4, 9, new String[] {"Restaurant"});
+//       PlaceManager.loadPlacesFromFile(map, "Cluster.csv",1000000);
+//       System.out.println(map.size());
+//        System.out.println(map.get(0));
+//        PlaceNode temp = map.get(0);
+//        System.out.println(map.search(2,6));
+        map.reset();
+
+//        System.out.println(temp.next);
+        System.out.println("Before remove: ");
+        while (map.hasNext()) {
+            System.out.println(map.next());
+        }
+        map.remove(1,4);
+        System.out.println("After remove: ");
+        while (map.hasNext()) {
+            System.out.println(map.next());
+        }
     }
 }
--- src/Map2D.java
+++ src/Map2D.java
@@ -2,27 +2,28 @@ import ADTs.ArrayQueue;
 import ADTs.LinkedList;
 
 public class Map2D extends LinkedList<PlaceNode> {
-    PlaceNode head;
-    public Map2D() {
-        this.head = null;
-    }
-
     public boolean add(String id, String name, int x, int y, String[] services) {
         Place newPlace = new Place(id, name, new Point(x, y), services);
+        reset();
+        if (!hasNext()) {
+            return insertAt(0, new PlaceNode(newPlace, null));
+        }
         return insertAt(size(), new PlaceNode(newPlace, null));
+//        return true;
     }
 
     public PlaceNode search(int x, int y) {
-        if (head == null) {
+        reset();
+        if (!hasNext()) {
             return null;
         }
         Point searchPoint = new Point(x, y);
-        PlaceNode temp = head;
-        while (temp != null) {
-            if (temp.place.getPoint().compareTo(searchPoint) == 0) {
-                return temp;
+        reset();
+        while (hasNext()) {
+            PlaceNode curr = next();
+            if (curr.place.getPoint().compareTo(searchPoint) == 0) {
+                return curr;
             }
-            temp = temp.next;
         }
         return null;
     }
@@ -54,7 +55,7 @@ public class Map2D extends LinkedList<PlaceNode> {
     }
 
     public ArrayQueue<Place> searchAvailable(int centerX, int centerY, int width, int height) {
-        if (head == null) return null;
+        if (!hasNext()) return null;
 
         int x1 = centerX - width / 2;
         int y1 = centerY - height / 2;
@@ -63,13 +64,13 @@ public class Map2D extends LinkedList<PlaceNode> {
 
         ArrayQueue<Place> result = new ArrayQueue<>();
 
-        PlaceNode current = head;
-        while (current != null) {
+        reset();
+        while (hasNext()) {
+            PlaceNode current = next();
             if (current.place.getPoint().getX() >= x1 && current.place.getPoint().getX() <= x2 &&
                     current.place.getPoint().getY() >= y1 && current.place.getPoint().getY() <= y2) {
                 result.enQueue(current.place);
             }
-            current = current.next;
         }
         return result;
     }
